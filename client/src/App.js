bad7fc6a00b9ba1f599e0367a157d42be472320b
Shubham Dutta <30752162+Shubham-Dutta@users.noreply.github.com>
2024-07-20 20:56:31 +0100

    Update App.js
    Added Popup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,65 @@
+import React, { useState } from 'react';
 import logo from './logo.png';
 import './App.css';
+import { Modal, Button } from 'react-bootstrap';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProject, setSelectedProject] = useState({});
+
+  const handleShow = (project) => {
+    setSelectedProject(project);
+    setShowModal(true);
+  };
+
+  const handleClose = () => setShowModal(false);
+
+  const projects = [
+    {
+      id: 1,
+      companyName: 'ABC',
+      projectBudget: '$50000',
+      bidMoney: '$2400',
+      projectDeadline: '26th January 2028',
+      deliverTargetDate: '16th December 2027',
+    },
+    {
+      id: 2,
+      companyName: 'DEF',
+      projectBudget: '$60000',
+      bidMoney: '$3400',
+      projectDeadline: '30th January 2028',
+      deliverTargetDate: '20th December 2027',
+    },
+    {
+      id: 3,
+      companyName: 'GHI',
+      projectBudget: '$70000',
+      bidMoney: '$4400',
+      projectDeadline: '10th February 2028',
+      deliverTargetDate: '25th December 2027',
+    },
+  ];
+
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className='container'>
+        <div className="container">
           <a className="navbar-brand" href="#">
             <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-            <span className='text-uppercase p-2 text-italic font-weight-bold'>Bid <span class="brand-color">Buddy</span></span>
+            <span className="text-uppercase p-2 text-italic font-weight-bold">
+              Bid <span className="brand-color">Buddy</span>
+            </span>
           </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
@@ -18,70 +67,64 @@ function App() {
               {/* <li className="nav-item active">
                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
               </li> */}
-            
             </ul>
           </div>
-          </div>
-        </nav>
+        </div>
+      </nav>
       <header className="App-header">
-        <div className='container-fluid'>
-        
-     <table class="table table-bordered">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">SL. No.</th>
-      <th scope="col">Company Name</th>
-      <th scope="col">Project Budget</th>
-      <th scope="col">Bid Money</th>
-      <th scope="col">Project Deadline</th>
-      <th scope="col">Deliver Target Date</th>
-      <th scope="col">Action</th>
+        <div className="container-fluid">
+          <table className="table table-bordered">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">SL. No.</th>
+                <th scope="col">Company Name</th>
+                <th scope="col">Project Budget</th>
+                <th scope="col">Bid Money</th>
+                <th scope="col">Project Deadline</th>
+                <th scope="col">Deliver Target Date</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {projects.map((project, index) => (
+                <tr key={project.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{project.companyName}</td>
+                  <td>{project.projectBudget}</td>
+                  <td>{project.bidMoney}</td>
+                  <td>{project.projectDeadline}</td>
+                  <td>{project.deliverTargetDate}</td>
+                  <td>
+                    <button
+                      className="btn btn-success btn-sm"
+                      onClick={() => handleShow(project)}
+                    >
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
-
-
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>ABC</td>
-      <td>$50000</td>
-      <td>$2400</td>
-      <td>26th January 2028</td>
-      <td>16th December 2027</td>
-      <td>
-      <button class = "btn btn-success btn-sm">View</button> 
-
-      </td>
-    </tr>
-   
-    <tr>
-      <th scope="row">2</th>
-      <td>ABC</td>
-      <td>$50000</td>
-      <td>$2400</td>
-      <td>26th January 2028</td>
-      <td>16th December 2027</td>
-      <td>
-      <button class = "btn btn-success btn-sm">View</button> 
-
-      </td>
-    </tr>  <tr>
-      <th scope="row">3</th>
-      <td>ABC</td>
-      <td>$50000</td>
-      <td>$2400</td>
-      <td>26th January 2028</td>
-      <td>16th December 2027</td>
-      <td>
-      <button class = "btn btn-success btn-sm">View</button> 
-
-      </td>
-    </tr>
-  </tbody>
-</table>
-</div>
-          
+        <Modal show={showModal} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Project Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p><strong>Company Name:</strong> {selectedProject.companyName}</p>
+            <p><strong>Project Budget:</strong> {selectedProject.projectBudget}</p>
+            <p><strong>Bid Money:</strong> {selectedProject.bidMoney}</p>
+            <p><strong>Project Deadline:</strong> {selectedProject.projectDeadline}</p>
+            <p><strong>Deliver Target Date:</strong> {selectedProject.deliverTargetDate}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </header>
     </div>
   );
